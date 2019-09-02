eafae1373a79ba6e95aa097b41f15240b601076f
GenaBitu <developer@dedic.eu>
2019-09-02 12:46:13 +0200

    Fixed some PHPCS issues, 5

--- src/php/functions.php
+++ src/php/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace CrdmBasic;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -10,86 +12,81 @@ define( 'CRDM_BASIC_TEMPLATE_URL', trailingslashit( get_stylesheet_directory_uri
 define( 'CRDM_BASIC_PARENT_TEMPLATE_PATH', realpath( get_template_directory() ) . DIRECTORY_SEPARATOR );
 define( 'CRDM_BASIC_PARENT_TEMPLATE_URL', trailingslashit( get_template_directory_uri() ) );
 
-class CrdmBasicTheme {
+function init() {
+	add_action( 'after_switch_theme', [ '\\CrdmBasic', 'switch_to_previous_theme_if_incompatible_version_of_wp_or_php' ] );
 
-	public function __construct() {
-		add_action( 'after_switch_theme', [ $this, 'switch_to_previous_theme_if_incompatible_version_of_wp_or_php' ] );
-
-		// if incompatible version of WP / PHP => don´t init
-		if ( ! $this->is_compatible_version_of_wp() || ! $this->is_compatible_version_of_php() ) {
-			return;
-		}
-
-		$this->init();
+	// if incompatible version of WP / PHP => don´t init
+	if ( ! is_compatible_version_of_wp() || ! is_compatible_version_of_php() ) {
+		return;
 	}
 
-	protected function init() {
-		require CRDM_BASIC_APP_PATH . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-		require CRDM_BASIC_APP_PATH . 'vendor' . DIRECTORY_SEPARATOR . 'aristath' . DIRECTORY_SEPARATOR . 'kirki' . DIRECTORY_SEPARATOR . 'kirki.php'; // init Kirki library
+	load();
+}
 
-		( new Crdm\Setup() );
-		if ( ! is_admin() ) {
-			( new Crdm\Front\Init() );
-		}
+function load() {
+	require CRDM_BASIC_APP_PATH . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+	require CRDM_BASIC_APP_PATH . 'vendor' . DIRECTORY_SEPARATOR . 'aristath' . DIRECTORY_SEPARATOR . 'kirki' . DIRECTORY_SEPARATOR . 'kirki.php'; // init Kirki library
+
+	( new \Crdm\Setup() );
+	if ( ! is_admin() ) {
+		( new \Crdm\Front\Init() );
 	}
+}
 
-	protected function is_compatible_version_of_wp() {
-		if ( isset( $GLOBALS['wp_version'] ) && version_compare( $GLOBALS['wp_version'], '4.9.8', '>=' ) ) {
-			return true;
-		}
-
-		return false;
-	}
-
-	protected function is_compatible_version_of_php() {
-		if ( version_compare( PHP_VERSION, '7.0', '>=' ) ) {
-			return true;
-		}
-
-		return false;
-	}
-
-	public function switch_to_previous_theme_if_incompatible_version_of_wp_or_php() {
-		if ( ! $this->is_compatible_version_of_php() || ! $this->is_compatible_version_of_wp() ) {
-			if ( ! $this->is_compatible_version_of_wp() ) {
-				add_action(
-					'admin_notices',
-					function () {
-						$this->show_admin_notice( esc_html__( '"CRDM - Basic" theme requires WordPress 4.9.8 or newer!', 'crdm-basic' ), 'warning' );
-					}
-				);
-			}
-
-			if ( ! $this->is_compatible_version_of_php() ) {
-				add_action(
-					'admin_notices',
-					function () {
-						$this->show_admin_notice( esc_html__( '"CRDM - Basic" theme requires PHP 7.0 or newer!', 'crdm-basic' ), 'warning' );
-					}
-				);
-			}
-
-			// Switch back to previous theme
-			switch_theme( get_option( 'theme_switched' ) );
-
-			return false;
-		}
-
+function is_compatible_version_of_wp() {
+	if ( isset( $GLOBALS['wp_version'] ) && version_compare( $GLOBALS['wp_version'], '4.9.8', '>=' ) ) {
 		return true;
 	}
 
-	public function show_admin_notice( $message, $type = 'warning' ) {
-		$class = 'notice notice-' . $type . ' is-dismissible';
-		printf(
-			'<div class="%1$s"><p>%2$s</p><button type="button" class="notice-dismiss">
-		<span class="screen-reader-text">' . esc_html__( 'Close', 'crdm-basic' ) . '</span>
-	</button></div>',
-			esc_attr( $class ),
-			$message
-		);
-	}
-
+	return false;
 }
 
-global $crdm_basic_theme;
-$crdm_basic_theme = new CrdmBasicTheme();
+function is_compatible_version_of_php() {
+	if ( version_compare( PHP_VERSION, '7.0', '>=' ) ) {
+		return true;
+	}
+
+	return false;
+}
+
+function switch_to_previous_theme_if_incompatible_version_of_wp_or_php() {
+	if ( ! is_compatible_version_of_php() || ! is_compatible_version_of_wp() ) {
+		if ( ! is_compatible_version_of_wp() ) {
+			add_action(
+				'admin_notices',
+				function () {
+					show_admin_notice( esc_html__( '"CRDM - Basic" theme requires WordPress 4.9.8 or newer!', 'crdm-basic' ), 'warning' );
+				}
+			);
+		}
+
+		if ( ! is_compatible_version_of_php() ) {
+			add_action(
+				'admin_notices',
+				function () {
+					show_admin_notice( esc_html__( '"CRDM - Basic" theme requires PHP 7.0 or newer!', 'crdm-basic' ), 'warning' );
+				}
+			);
+		}
+
+		// Switch back to previous theme
+		switch_theme( get_option( 'theme_switched' ) );
+
+		return false;
+	}
+
+	return true;
+}
+
+function show_admin_notice( $message, $type = 'warning' ) {
+	$class = 'notice notice-' . $type . ' is-dismissible';
+	printf(
+		'<div class="%1$s"><p>%2$s</p><button type="button" class="notice-dismiss">
+	<span class="screen-reader-text">' . esc_html__( 'Close', 'crdm-basic' ) . '</span>
+</button></div>',
+		esc_attr( $class ),
+		$message
+	);
+}
+
+init();
