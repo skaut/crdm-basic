afbf8846e0963d3bbe10ab89c59163fb02703ff1
GenaBitu <developer@dedic.eu>
2019-09-05 07:54:07 -0500

    eslint fixes

--- src/js/frontend/utils.js
+++ src/js/frontend/utils.js
@@ -1,17 +1,18 @@
 'use strict';
 
 function browserSupportCssVariables() {
+    var styles, doesSupport;
     var color = 'rgb(255, 198, 0)';
-    var el = document.createElement('span');
+    var el = document.createElement( 'span' );
 
-    el.style.setProperty('--color', color);
-    el.style.setProperty('background', 'var(--color)');
-    document.body.appendChild(el);
+    el.style.setProperty( '--color', color );
+    el.style.setProperty( 'background', 'var(--color)' );
+    document.body.appendChild( el );
 
-    var styles = getComputedStyle(el);
-    var doesSupport = styles.backgroundColor === color;
-    document.body.removeChild(el);
+    styles = getComputedStyle( el );
+    doesSupport = styles.backgroundColor === color;
+    document.body.removeChild( el );
     return doesSupport;
 }
 
-export {log, intval, isTouchDevice, browserSupportCssVariables};
+export {browserSupportCssVariables};
