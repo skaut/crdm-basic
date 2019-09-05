2bd05da69170b9652ea0a6ed7a70d2324a3fbef2
GenaBitu <developer@dedic.eu>
2019-09-05 07:53:10 -0500

    Removed unused code

--- src/js/frontend/utils.js
+++ src/js/frontend/utils.js
@@ -1,26 +1,5 @@
 'use strict';
 
-function log(msg) {
-    return console.log(msg);
-}
-
-function intval(v) {
-    v = parseInt(v);
-    return isNaN(v) ? 0 : v;
-}
-
-function isTouchDevice() {
-    return (
-        !!(typeof window !== 'undefined' &&
-            ('ontouchstart' in window ||
-                (window.DocumentTouch &&
-                    typeof document !== 'undefined' &&
-                    document instanceof window.DocumentTouch))) ||
-        !!(typeof navigator !== 'undefined' &&
-            (navigator.maxTouchPoints || navigator.msMaxTouchPoints))
-    );
-}
-
 function browserSupportCssVariables() {
     var color = 'rgb(255, 198, 0)';
     var el = document.createElement('span');
