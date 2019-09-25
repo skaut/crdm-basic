14d8f3690c4e39aa327127dffeca9acdac3267da
Marek Dědič <developer@dedic.eu>
2019-09-25 15:44:25 +0200

    Added Background_Image_Customize_Control

---
+++ .phan/stubs/WordPress/class-wp-control.php
@@ -0,0 +1,18 @@
+<?php
+
+class WP_Customize_Control {
+	public $json;
+	public $settings;
+
+	public function __construct( $a, $b, $c ) {
+	}
+
+	public function get_link( $a ) {
+	}
+
+	public function to_json() {
+	}
+
+	public function value( $a ) {
+	}
+}
